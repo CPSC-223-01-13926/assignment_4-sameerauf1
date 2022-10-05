5e76e0fb29c5d61cbd49d1645a07dea4765849fb
Samee Rauf <sameerauf1@gmail.com>
2022-10-05 09:45:20 -0700

    10/5

---
+++ weather.py
@@ -0,0 +1,81 @@
+def read_data(filename):
+    try:
+        with open(filename, 'r') as f:  # read only
+            return json.load(f)
+    except FileNotFoundError:
+        return {}
+
+
+def write_data(data, filename):
+    with open(filename, 'w') as f:
+        json.dump(data, f)
+
+
+def max_temperature(data, date):  # data is dict
+    x = 0  # temp in celsius
+    for key in data:  # key: date, value: temp
+        if date == key[0:8]:
+            if date[key]['t'] > x:
+                x = data[key]['t']
+    return x
+
+
+def min_temperature(data, date):
+    x = 9999
+    for key in data:
+        if date == key[0:8]:
+            if data[key]['t'] < x:
+                x = data[key]['t']
+    return x
+
+
+def max_humidity(data, date):
+    x = 0
+    for key in data:
+        if date == key[0:8]:
+            if data[key]['h'] > x:
+                x = data[key]['h']
+    return x
+
+
+def min_humidity(data, date):
+    x = 9999
+    for key in data:
+        if date == key[0:8]:
+            if data[key]['h'] < x:
+                x = data[key]['h']
+    return x
+
+
+def tot_rain(data, date):
+    sum = 0
+    for key in data:
+        if date == key[0:8]:
+            sum = sum + data[key]['r']
+    return sum
+
+
+def report_daily(data, date):
+    display = "========================= DAILY REPORT ======================== \n"
+    display = display + "Date                      Time  Temperature  Humidity  Rainfall\n"
+    display = display + "====================  ========  ===========  ========  ========\n"
+    for key in data:
+        if date == key[0:8]:
+            m = calendar.month_name(
+                int(date[4:6])) + " " + str(int(date[6:8])) + "," + str(int(date[0:4]))
+            tm = key[8:10] + ":" + key[10:12] + ":" + key[12:14]  # time
+            t = date[key]['t']
+            h = date[key]['h']
+            r = date[key]['r']
+            display = display + \
+                f'{m:22}{tm:8}{t:13}{h:10}{r:10:2f} \n'  # potential bug
+    return display
+
+
+def report_historical(data):
+    display = " 	============================== HISTORICAL REPORT ===========================\n"
+    display += "				  Minimum      Maximum   Minumum   Maximum     Total\n"
+    display += "	Date                  Temperature  Temperature  Humidity  Humidity  Rainfall\n"
+    display += "	====================  ===========  ===========  ========  ========  ========\n"
+    for key in data:
+        
